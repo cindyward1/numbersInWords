e5e6d476a93337eeb2a0b855fc30b985e04c26b9
Andrew M Westling & Cindy Ward <student@epicodus.com>
2014-07-16 12:23:54 -0700

    Application complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 var splitter = function(numberString){
-  // var numberString = numberInput.toString();
-  console.log(numberString);
+  //result [[1,2,3],[4,5,6],[7,8,9]]
+
   var numberArray = numberString.split("").map(Number);
   var bigArray = [];
   var originalLength = numberArray.length;
 
   for (var i=0; i<originalLength; i+=3) {
+    bigArray.push(numberArray.slice(-3));
+    numberArray.splice(((numberArray.length)-3), 3);
+  };
 
-  bigArray.push(numberArray.slice(-3));
-  numberArray.splice(((numberArray.length)-3), 3);
-}
   return bigArray.reverse();
 
-  //result [[1,2,3],[4,5,6],[7,8,9]]
-}
+};
 
 var numbersInWords = function(numberString){
   // var numberString = numberInput.toString();
   // var numberArray = numberString.split("").map(Number);
   var bigArray = splitter(numberString);
   var finalArray = [];
+  var powerNum = 0;
 
 
   var onesWords = { 0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine" };
@@ -28,32 +28,66 @@ var numbersInWords = function(numberString){
 
   var tensWords = { 2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety" };
 
+  var powerWords = {1000:"thousand",1000000:"million",1000000000:"billion",
+                    1000000000000:"trillion"};
 
   for (var i=0; i<bigArray.length; i++) {
 
     var l = bigArray[i].length;
+    powerNum = 0;
 
-    if (l === 3) {    //hundreds
+    if (bigArray.length === 1 && bigArray[i][0] === 0) { // special case of zero input
+      finalArray.push("zero");
+      break;
+    };
+
+    if (l === 3 && bigArray[i][0] !== 0) {    //hundreds
       finalArray.push(onesWords[bigArray[i][0]] + " " + "hundred");
-    }
-    // } else if (l === 3 && bigArray[i][1] === 0)
+    };
 
-    if (bigArray[i][l-2] >= 2) { //20-99
+    if (bigArray[i][l-2] >= 2 && bigArray[i][l-1] !== 0) { //20-99
       finalArray.push(tensWords[bigArray[i][l-2]] + " " + onesWords[bigArray[i][l-1]]);
-
+    } else if (bigArray[i][l-2] >= 2 && bigArray[i][l-1] === 0) { //20-99
+      finalArray.push(tensWords[bigArray[i][l-2]]);
     } else if (bigArray[i][l-2] === 1) { //teens
       var teenNumber = "1" + bigArray[i][l-1];
       var teenNumberInt = parseInt(teenNumber);
       finalArray.push(teensWords[teenNumberInt]);
-
-    } else if (l=1 && bigArray[i][0] === 0) {     //zero
-      finalArray.push("zero");
-
-    } else if (l = 1) {   //ones
-      finalArray.push(onesWords[bigArray[i][0]]);
+    } else if (bigArray[i][l-2] === 0) {
+      if (bigArray[i][l-1] !== 0) {   //ones
+        finalArray.push(onesWords[bigArray[i][l-1]]);
+      };
+    } else if (l = 1 && bigArray[i][0] !== 0) {   //ones
+      finalArray.push(onesWords[bigArray[i][l-1]]);
     };
-   // console.log(finalArray);
 
-   return finalArray.join(" ");
-}
-}
+    // write out order of magnitude
+    powerNum = Math.pow(10,((bigArray.length-i-1) * 3));
+    if (powerNum >= 1000 && (bigArray[i][0] !== 0 || bigArray[i][1] !== 0 || bigArray[i][2] !== 0)) {
+    finalArray.push(powerWords[powerNum]);
+    };
+
+  }; // end for loop
+
+  console.log(finalArray);
+  return finalArray.join(" ");
+
+}; // end function
+
+$(document).ready (function () {
+
+  $("form#input-form").submit (function (event) {
+
+    var inputNumberString = $("input#input").val();
+    var inputNumberString2 = inputNumberString.match(/\d+/g).join("");
+    var outputString = numbersInWords(inputNumberString2);
+
+    $(".output").text(outputString);
+    $("#result").show();
+
+    event.preventDefault();
+
+  });
+
+});
+
--- spec/specs.js
+++ spec/specs.js
@@ -33,10 +33,46 @@ describe("numbersInWords", function() {
     numbersInWords("239").should.equal('two hundred thirty nine');
   });
   it("returns the correct string for numbers with 4 or more digits", function() {
-    numbersInWords("1234").should.equal('one two hundred thirty four');
+    numbersInWords("1234").should.equal('one thousand two hundred thirty four');
   });
   it("returns the correct string for numbers with 4 or more digits", function() {
-    numbersInWords("1234567").should.equal('one two hundred thirty four five hundred sixty seven');
+    numbersInWords("1234567").should.equal('one million two hundred thirty four thousand five hundred sixty seven');
+  });
+    it("should display 'zero' for the entered value of 0", function() {
+    numbersInWords("0").should.equal("zero");
+  });
+  it("should display 'one' for the entered value of 1", function() {
+    numbersInWords("1").should.equal("one");
+  });
+  it("should display 'twenty nine' for the entered value of 29", function() {
+    numbersInWords("29").should.equal("twenty nine");
+  });
+  it("should display 'three hundred' for the entered value of 300", function() {
+    numbersInWords("300").should.equal("three hundred");
+  });
+  it("should display 'three hundred twenty six' for the entered value of 326", function() {
+    numbersInWords("326").should.equal("three hundred twenty six");
+  });
+  it("should display 'one thousand forty three' for the entered value of 1043", function() {
+    numbersInWords("1043").should.equal("one thousand forty three");
+  });
+  it("should display 'one thousand one hundred forty three' for the entered value of 1143", function() {
+    numbersInWords("1143").should.equal("one thousand one hundred forty three");
+  });
+  it("should display 'nine thousand one hundred forty three' for the entered value of 9143", function() {
+    numbersInWords("9143").should.equal("nine thousand one hundred forty three");
+  });
+  it("should display 'eleven thousand two hundred three' for the entered value of 11203", function() {
+    numbersInWords("11203").should.equal("eleven thousand two hundred three");
+  });
+  it("should display 'one hundred thousand three' for the entered value of 100003", function() {
+    numbersInWords("100003").should.equal("one hundred thousand three");
+  });
+  it("should display 'one hundred forty thousand three' for the entered value of 140003", function() {
+    numbersInWords("140003").should.equal("one hundred forty thousand three");
+  });
+  it("should display 'one billion two' for the entered value of 1000000002", function() {
+    numbersInWords("1000000002").should.equal("one billion two");
   });
 });
 
